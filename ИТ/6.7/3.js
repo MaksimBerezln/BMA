61591e476ef1591b6b8ab23a7008e23a840f7b2a
MaksimBerezln <https://github.com/MaksimBerezln/BMA>
2021-05-22 18:28:20 +0300

    22.05.2021

---
+++ ИТ/6.7/3.js
@@ -0,0 +1,12 @@
+let arr=[5,3,8,1];
+function filterRangeInPlace(arr,a,b){
+  for(let i=0;i<arr.length;i++){
+    let val=arr[i];
+    if(val<a||val>b){
+      arr.splice(i,1);
+      i--;
+    }
+  }
+}
+filterRangeInPlace(arr,1,4);
+alert(arr);
---
+++ ИТ/6.7/4.js
@@ -0,0 +1,3 @@
+const array1 = [5,2,1,-10,8];
+const reversed = array1.reverse();
+alert( reversed);
